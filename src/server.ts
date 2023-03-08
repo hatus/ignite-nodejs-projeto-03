336781861d788644d9ee520fe4206205af9ee465
Hatus Níwman <hatusn@gmail.com>
2023-03-08 16:45:17 -0300

    Carregando variaveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import { app } from "./app";
+import { app } from './app';
+import { env } from './env';
 
-app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
-	console.log(`Listen on 3333`);
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
+  console.log(`Listen on ${env.PORT}`);
 });
