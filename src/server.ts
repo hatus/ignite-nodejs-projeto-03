b36661a3e57d7dc108bf2b111fd8df3cf6b15637
Hatus Níwman <hatusn@gmail.com>
2023-03-09 12:24:21 -0300

    Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import { app } from './app';
-import { env } from './env';
+import { app } from './app'
+import { env } from './env'
 
 app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
-  console.log(`Listen on ${env.PORT}`);
-});
+  console.log(`Listen on ${env.PORT}`)
+})
