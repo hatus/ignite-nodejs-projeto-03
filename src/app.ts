1efcdbe1bbf87d5138b716a2d7e2037fdae81d24
Hatus Níwman <hatusn@gmail.com>
2023-03-11 14:20:31 -0300

    Utilizando o Docker Compose

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,30 @@ import fastify from 'fastify'
 export const app = fastify()
 
 const prisma = new PrismaClient()
-prisma.user.create({
-  data: {
-    name: 'hatus niwman',
-    email: 'hatus@gmail.com',
-  },
-})
+
+async function createuser(name: string, email: string) {
+  const user = await prisma.user.create({ data: { email, name } })
+
+  return user
+}
+
+async function deleteuser(email: string) {
+  const deletedUser = await prisma.user.delete({ where: { email } })
+  return deletedUser
+}
+
+deleteuser('hatusn@gmail.com')
+  .then((user) => {
+    console.log(`${user.name} apagado com sucesso`)
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
+
+createuser('hatus', 'hatusn@gmail.com')
+  .then((user) => {
+    console.log(user.name)
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
