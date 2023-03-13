420c280f51c5bd702ced6f23c4def021f7127c58
Hatus Níwman <hatusn@gmail.com>
2023-03-13 16:07:26 -0300

    Inversao de dependencias

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,7 +14,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({ email, name, password })
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({ email, name, password })
   } catch (error) {
     return reply.status(409).send()
   }
