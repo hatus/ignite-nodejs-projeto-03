5bcaa105c353916ef48155d7a731499a40a597c9
Hatus Níwman <hatusn@gmail.com>
2023-05-19 16:26:59 -0300

    Validando horario do check-in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -3,6 +3,7 @@ import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 let checkInsRepo: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
@@ -12,11 +13,11 @@ describe('Validate Check-In Use Case', () => {
     checkInsRepo = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepo)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to valiadte a check-in', async () => {
@@ -33,11 +34,42 @@ describe('Validate Check-In Use Case', () => {
     expect(checkInsRepo.items[0].validated_at).toEqual(expect.any(Date))
   })
 
-  it('should not be able to valiadte an inexistent check-in', async () => {
+  it('should not be able to validate an inexistent check-in', async () => {
     await expect(() =>
       sut.execute({
         checkInId: 'inexistent-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutos of its creation', async () => {
+    /**
+     * Define a hora do sistema para essa abaixo.
+     *
+     * Ao criar um novo checkIn com checkInsRepo.create(),
+     * o new Date() vai pegar a data criada abaixo.
+     */
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40)) // em UTC
+
+    const createdCheckIn = await checkInsRepo.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    // 21 minutos em milisegundos
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    /**
+     * Avança na data criado acima o valor abaixo em milisegundos.
+     * Com a constante twentyOneMinutesInMs, o novo valor da hora do sistema
+     * é 14:01 (adicionou 21 minutos)
+     */
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,7 +1,9 @@
 import { CheckIn } from '@prisma/client'
+import dayjs from 'dayjs'
 
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -21,6 +23,23 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    /**
+     * days().diff() - retorna a diferença entre duas datas
+     *
+     * Ex: dayjs(new Date()).diff(checkIn.created_at, 'minutes')
+     *
+     * A função acima retorna a diferença, em minutos, entre a data atual (new Date())
+     * e a data que o check-in foi criado (.created_at)
+     */
+    const distanceInMinutosFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutosFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
     }
 
     checkIn.validated_at = new Date()
