ca397d003cfa907fd2daf74c468819a61d19a9e9
Hatus Níwman <hatusn@gmail.com>
2023-05-21 18:18:12 -0300

    Principios da autenticacao JWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,8 +2,14 @@ import { FastifyInstance } from 'fastify'
 
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /**
+   * Authenticated routes
+   */
+  app.get('/me', profile)
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+import { FastifyListenOptions } from 'fastify'
+
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
+const opts: FastifyListenOptions = {
+  host: '0.0.0.0',
+  port: env.PORT,
+}
+
+app.listen(opts).then(() => {
   console.log(`HTTP Server listening on port ${env.PORT}`)
 })
