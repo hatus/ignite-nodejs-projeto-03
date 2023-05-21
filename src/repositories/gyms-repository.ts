4792cfbab1df91c49bdc52ef41e32bc6d349c8bd
Hatus Níwman <hatusn@gmail.com>
2023-05-21 13:51:44 -0300

    Repositorio de academias

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -6,7 +6,7 @@ export interface FindManyNearbyParams {
 }
 
 export interface GymsRepository {
-  findById(id: String): Promise<Gym | null>
+  findById(id: string): Promise<Gym | null>
   create(data: Prisma.GymCreateInput): Promise<Gym>
   searchMany(query: string, page: number): Promise<Gym[]>
   findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
