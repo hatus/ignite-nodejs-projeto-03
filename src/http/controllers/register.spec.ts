9cb263bf130d4bbefd8791904130b5e42534ad37
Hatus Níwman <hatusn@gmail.com>
2023-05-25 16:00:30 -0300

    Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {
-  console.log('teste ok')
+describe('Register User (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'john doe',
+      email: 'john@example.com',
+      password: '123123',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
